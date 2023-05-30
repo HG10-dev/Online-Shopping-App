f4d0b3a8acc2638d0f6a61bbb7f2a3820b5480c3
gauta <gautamhimangshu@gmail.com>
2023-05-30 20:30:50 +0530

    Unit testing for authentication

---
+++ Authorization_Microservices_UnitTests/AuthRepoTest.cs
@@ -0,0 +1,62 @@
+﻿using Authorization_Microservice.Models;
+using Authorization_Microservice.Repositories;
+using Authorization_Microservice.Services;
+//using Castle.Core.Configuration;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authorization_Microservices_UnitTests
+{
+    internal class AuthServiceTest
+    {
+        //private List<AuthCredentials> credentials;
+
+        [SetUp]
+        public void Setup()
+        {
+            //credentials = new List<AuthCredentials>
+            //{
+            //    new AuthCredentials { Id = "id_01", Username = "admin", Password = "password", IsAdmin = true },
+            //    new AuthCredentials { Id = "id_02", Username = "customer", Password = "password", IsAdmin = false }
+            //};
+        }
+
+        [TestCase("id_01", "admin", "password",true)]
+        [TestCase("id_02", "customer", "password",false)]
+        public void GenerateJwtReturnsToken(string id, string uname, string pass, bool isadmin)
+        {
+            AuthCredentials credential = new AuthCredentials
+            {
+                Id = id,
+                Username = uname,
+                Password = pass,
+                IsAdmin = isadmin
+            };
+            Mock<IConfiguration> mock = new Mock<IConfiguration>();
+            mock.SetupGet(x => x[It.Is<string>(s => s == "Jwt:Key")]).Returns("16bitsecuritykey");
+            mock.SetupGet(x => x[It.Is<string>(s => s == "Jwt:Issuer")]).Returns("issuer");
+
+            AuthRepo authRepo = new AuthRepo(mock.Object);
+            string? token = authRepo.GenerateJWT(credential);
+            Assert.IsNotNull(token);
+        }
+        
+        public void GenerateJwtReturnsNull()
+        {
+            AuthCredentials credential=null;
+            Mock<IConfiguration> mock = new Mock<IConfiguration>();
+            mock.SetupGet(x => x[It.Is<string>(s => s == "Jwt:Key")]).Returns("16bitsecuritykey");
+            mock.SetupGet(x => x[It.Is<string>(s => s == "Jwt:Issuer")]).Returns("issuer");
+
+            AuthRepo authRepo = new AuthRepo(mock.Object);
+            string? token = authRepo.GenerateJWT(credential);
+            Assert.IsNull(token);
+        }
+    }
+}
---
+++ Authorization_Microservices_UnitTests/AuthenticateControllerTest.cs
@@ -0,0 +1,66 @@
+﻿using Authorization_Microservice.Controllers;
+using Authorization_Microservice.Models;
+using Authorization_Microservice.Repositories;
+using Authorization_Microservice.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authorization_Microservices_UnitTests
+{
+    internal class AuthenticateControllerTest
+    {
+        public async Task LoginReturnsBadRequest()
+        {
+            AuthCredentials? cred = null;
+            Mock<IAuthRepo> mockRepo = new Mock<IAuthRepo>();
+            //mockRepo.Setup(r => r.GenerateJWT(cred)).Returns("token");
+            Mock<IAuthService> mockService = new Mock<IAuthService>();
+            //mockService.Setup(s => s.GetAuthCredentialsAsync("name", "pass")).ReturnsAsync(cred);
+            AuthenticateController controller = new AuthenticateController(mockRepo.Object, mockService.Object);
+            ObjectResult? result = await controller.Login(cred) as ObjectResult;
+            Assert.That(result.StatusCode, Is.EqualTo(400));
+        }
+
+        public async Task LoginReturnsUnauthorized()
+        {
+            AuthCredentials cred = new AuthCredentials() { Id = "id", Username = "name", Password = "pas", IsAdmin = true };
+            Mock<IAuthRepo> mockRepo = new Mock<IAuthRepo>();
+            mockRepo.Setup(r => r.GenerateJWT(cred)).Returns("token");
+            Mock<IAuthService> mockService = new Mock<IAuthService>();
+            AuthCredentials? temp=null;
+            mockService.Setup(s => s.GetAuthCredentialsAsync("name", "pass")).ReturnsAsync(temp);
+            AuthenticateController controller = new AuthenticateController(mockRepo.Object, mockService.Object);
+            ObjectResult? result = await controller.Login(cred) as ObjectResult;
+            Assert.That(result.StatusCode, Is.EqualTo(401));
+        }
+        public async Task LoginSuccessfull()
+        {
+            AuthCredentials cred = new AuthCredentials(){Id="id", Username="name", Password="pas", IsAdmin=true};
+            Mock<IAuthRepo> mockRepo = new Mock<IAuthRepo>();
+            mockRepo.Setup(r => r.GenerateJWT(cred)).Returns("token");
+            Mock<IAuthService> mockService = new Mock<IAuthService>();
+            mockService.Setup(s => s.GetAuthCredentialsAsync("name", "pass")).ReturnsAsync(cred);
+            AuthenticateController controller = new AuthenticateController(mockRepo.Object, mockService.Object);
+            ObjectResult? result = await controller.Login(cred) as ObjectResult;
+            Assert.That(result.StatusCode, Is.EqualTo(200));
+        }
+
+        public async Task LoginFailstoGenerateJwtAndReturnsServerError()
+        {
+            AuthCredentials cred = new AuthCredentials() { Id = "id", Username = "name", Password = "pas", IsAdmin = true };
+            Mock<IAuthRepo> mockRepo = new Mock<IAuthRepo>();
+            mockRepo.Setup(r => r.GenerateJWT(cred)).Throws(new Exception("Something went wrong"));
+            Mock<IAuthService> mockService = new Mock<IAuthService>();
+            mockService.Setup(s => s.GetAuthCredentialsAsync("name", "pass")).ReturnsAsync(cred);
+            AuthenticateController controller = new AuthenticateController(mockRepo.Object, mockService.Object);
+            ObjectResult? result = await controller.Login(cred) as ObjectResult;
+            Assert.That(result.StatusCode, Is.EqualTo(500));
+        }
+    }
+}
